55d6d10c31f87a49e0d8b4eea9036f2e759d8d38
kamehamehaa0000 <gayush794@gmail.com>
2024-08-07 22:33:13 +0530

    multer changes

--- backend/src/config/multer.ts
+++ backend/src/config/multer.ts
@@ -1,16 +1,34 @@
 import multer from 'multer'
+import path from 'path'
+import os from 'os'
+
+const tempDir = os.tmpdir()
 
 const diskStorage = multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, './public/temp')
+    callback(null, tempDir)
   },
   filename: function (req, file, callback) {
-    callback(null, file.originalname)
+    callback(null, Date.now() + path.extname(file.originalname))
   },
 })
 
 const upload = multer({
   storage: diskStorage,
+  limits: { fileSize: 10 * 1024 * 1024 },
+  fileFilter: (req, file, callback) => {
+    const filetypes = /jpeg|jpg|png|gif/
+    const mimetype = filetypes.test(file.mimetype)
+    const extname = filetypes.test(
+      path.extname(file.originalname).toLowerCase()
+    )
+
+    if (mimetype && extname) {
+      return callback(null, true)
+    } else {
+      callback(new Error('Only images are allowed!'))
+    }
+  },
 })
 
 export default upload
